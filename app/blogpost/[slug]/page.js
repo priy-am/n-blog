66b959ef21c460875f8666f8574dc4ff7d6076e0
priy-am <priyam2101753@gmail.com>
2025-02-25 03:29:10 +0530

    edit somthing

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -1,5 +1,4 @@
 import fs from "fs";
-import path from "path";
 import matter from "gray-matter";
 import { notFound } from "next/navigation";
 import rehypeDocument from "rehype-document";
@@ -9,60 +8,51 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
 import rehypePrettyCode from "rehype-pretty-code";
-import { transformerCopyButton } from "@rehype-pretty/transformers";
+import { transformerCopyButton } from '@rehype-pretty/transformers'
 import OnThisPage from "@/components/onthispage";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
-
-export async function generateStaticParams() {
-  const contentDir = path.join(process.cwd(), "content");
-  const files = fs.readdirSync(contentDir);
-
-  return files.map((file) => ({
-    slug: file.replace(".md", ""),
-  }));
-}
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
 
 export default async function Page({ params }) {
-  const filePath = path.join(process.cwd(), "content", `${params.slug}.md`);
 
+
+  const filePath = `content/${params.slug}.md`;
   if (!fs.existsSync(filePath)) {
     notFound();
     return;
   }
-
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { content, data } = matter(fileContent);
-
+  // console.log(data, content)
   const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeDocument, { title: data.title || "Blog Post" })
+    .use(rehypeDocument, { title: "👋🌍" })
     .use(rehypeFormat)
     .use(rehypeStringify)
     .use(rehypeSlug)
     .use(rehypeAutolinkHeadings)
     .use(rehypePrettyCode, {
-      theme: "github-dark",
+      theme:'github-dark', 
       transformers: [
         transformerCopyButton({
-          visibility: "always",
-          feedbackDuration: 3000,
+          visibility: 'always',
+          feedbackDuration: 3_000,
         }),
       ],
-    });
+    })
 
   const htmlContent = (await processor.process(content)).toString();
 
   return (
-    <div className="mx-auto max-w-5xl p-6 rounded-lg shadow-lg">
+    <div className="mx-auto max-w-5xl p-6  rounded-lg shadow-lg">
       <h2 className="text-4xl font-bold mb-4">{data.title}</h2>
-      <p className="text-base mb-2 border-l-4 border-gray-500 pl-4 italic">
-        &quot;{data.description}&quot;
+      <p className="text-base mb-2 border-l-4 border-gray-500 pl-4 italic ">
+        &quot;{data.description} &quot;{" "}
       </p>
-      <div className="flex gap-2">
-        <p className="text-sm mb-4 italic">by {data.author}</p>
-        <p className="text-sm mb-4">{data.date}</p>
+      <div className="flex gap-2 ">
+        <p className="text-sm  mb-4 italic "> by {data.author}</p>
+        <p className="text-sm  mb-4">{data.date}</p>
       </div>
       <div
         className="prose dark:prose-invert"
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: false,
     reactStrictMode: true,
-  };
-  
-  export default nextConfig;
-  
+    async rewrites() {
+        return [
+            {
+                source: "/content/:path*",
+                destination: "/_next/static/content/:path*",
+            },
+        ];
+    },
+    experimental: {
+        outputFileTracingRoot: path.join(process.cwd(), "../../"),
+    },
+};
+
+export default nextConfig;
